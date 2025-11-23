b05f8afdb2b772254ab4c2665d3d4f09363ed82d
Federicolx <federicolx@outlook.com>
2025-11-23 19:42:03 +0800

    Change Python file names and add study notes from the 23rd

--- 2025/11_November/2025-11-23_character_count_23.py
+++ 2025/11_November/2025-11-23_character_count_23.py
@@ -37,4 +37,3 @@ print(count_characters("I love coding challenges!"))
  # ["a 1", "c 2", "d 1", "e 3", "g 2", "h 1", "i 2", "l 3", "n 2", "o 2", "s 1", "v 1"]
 print(count_characters("// TODO: Complete this challenge ASAP!"))
 # ["a 3", "c 2", "d 1", "e 4", "g 1", "h 2", "i 1", "l 3", "m 1", "n 1", "o 3", "p 2", "s 2", "t 3"]
-
