a9485b9d9b44e8be40851ced3ff409b373f93e03
Federicolx <federicolx@outlook.com>
2025-12-03 09:01:27 +0800

    Add 2025-12-03 daily challenge:Miles to Kilometers and change the file title of Camel to Snake

---
+++ 2025/12_December/2025-12-2_miles_to_kilometers.py
@@ -0,0 +1,29 @@
+'''
+Title:Miles to Kilometers
+Date:2025-12-3
+Source: https://www.freecodecamp.org/learn/daily-coding-challenge/2025-12-01
+Given a distance in miles as a number, return the equivalent distance in kilometers.
+
+The input will always be a non-negative number.
+1 mile equals 1.60934 kilometers.
+Round the result to two decimal places.
+
+Examples:
+1. convert_to_km(1) should return 1.61.
+Waiting:2. convert_to_km(21) should return 33.8.
+Waiting:3. convert_to_km(3.5) should return 5.63.
+Waiting:4. convert_to_km(0) should return 0.
+Waiting:5. convert_to_km(0.621371) should return 1.
+'''
+
+def convert_to_km(miles):
+    if miles <= 0:
+        return 0
+    else:
+        km = miles * 1.60934
+        km = round(km,2)
+        return km
+convert_to_km(1)
+convert_to_km(3.5)
+convert_to_km(0)
+convert_to_km(0.621371)
